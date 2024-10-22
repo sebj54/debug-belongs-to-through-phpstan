da2279f179409aff6f989ad3deaab87dd223b765
Sébastien JEAN <contact@sebjean.com>
2024-10-22 11:16:15 +0200

    ✅ Add tests on BelongsToThrough relations

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,15 +2,47 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Comment;
+use App\Models\Country;
+use App\Models\Post;
+use App\Models\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
+    use RefreshDatabase;
+
+    public function test_post_country_relation(): void
     {
-        $this->assertTrue(true);
+        $country = Country::factory()->create();
+        $user = User::factory()
+            ->for($country)
+            ->create();
+
+        $post = Post::factory()
+            ->for($user)
+            ->create();
+
+        $this->assertEquals($country->id, $post->country->id);
+    }
+
+    public function test_comment_country_relation(): void
+    {
+        $country = Country::factory()->create();
+        $user = User::factory()
+            ->for($country)
+            ->create();
+
+        $post = Post::factory()
+            ->for($user)
+            ->create();
+
+        $comment = Comment::factory()
+            ->for($post)
+            ->for($user)
+            ->create();
+
+        $this->assertEquals($country->id, $comment->country->id);
     }
 }
